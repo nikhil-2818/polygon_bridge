403503208d2ae50206eab877b3d68ab9e3220c82
nikhil-2818 <156065283+nikhil-2818@users.noreply.github.com>
2024-07-08 17:13:12 +0530

    deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
 
-    const MetaNFT = await hre.ethers.getContractFactory("MyNFT");
-    const MyNFT = await MetaNFT.deploy();
+  const token = await hre.ethers.deployContract("MyNFT");
 
-    await MyNFT.deployed();
-    console.log("nfts deployed to:", MyNFT.address);
+  console.log("Token address:", await token.getAddress());
 }
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
