63237093e525d2642e2980ccc776bf6a3dd9e140
nikhil-2818 <156065283+nikhil-2818@users.noreply.github.com>
2024-07-08 21:46:49 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: process.env.SEP_TEST_RPC,
+      url: process.env.SEP_RPC,
       chainId:11155111,
       accounts:[process.env.SEP_PRIV_KEY]
     },
